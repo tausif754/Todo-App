31a93754ae692b11ee940a185b4c82565a8e1d89
Tausif Ahmad <tausifahmad754@gmail.com>
2024-11-20 10:19:22 +0530

    All file setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Todo from "./components/Todo";
+
+const App = () => {
+  return (
+    <div className="bg-stone-900 grid min-h-screen py-4">
+      <Todo />
+    </div>
+  );
+};
+
+export default App;
