f101bc1570b5e91e0939c366f5e5974695a3c551
Tausif Ahmad <tausifahmad754@gmail.com>
2024-11-21 00:34:42 +0530

    Added complete and incomplete functionality

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -3,12 +3,23 @@ import tick from "../assets/tick.png";
 import not_tick from "../assets/not_tick.png";
 import delete_icon from "../assets/delete.png";
 
-const TodoItems = ({ text, id, isComplete, deleteTodo }) => {
+const TodoItems = ({ text, id, isComplete, deleteTodo, toggle }) => {
   return (
     <div className="flex items-center my-3 gap-2">
-      <div className="flex flex-1 items-center cursor-pointer">
-        <img className="w-7" src={tick} alt="" />
-        <p className="text-slate-700 ml-4 text-[17px]">{text}</p>
+      <div
+        onClick={() => {
+          toggle(id);
+        }}
+        className="flex flex-1 items-center cursor-pointer"
+      >
+        <img className="w-7" src={isComplete ? tick : not_tick} alt="" />
+        <p
+          className={`text-slate-700 ml-4 text-[17px] decoration-slate-500 ${
+            isComplete ? "line-through" : ""
+          }`}
+        >
+          {text}
+        </p>
       </div>
 
       <div>
